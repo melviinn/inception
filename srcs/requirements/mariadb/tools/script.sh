b7728367609034d59c78adabb20d0c3d65749c8f
Melvin Duchaufour <mduchauf@student.42.fr>
2026-02-03 16:35:18 +0100

    add wordpress to the docker-compose file && start his Dockerfile

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -60,8 +60,8 @@ if [ "$is_db_created" -eq 1 ]; then
   log_ok "Creating database/user..."
   mysql --protocol=socket --socket="$SOCKET" -u root <<SQL
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 SQL
@@ -77,3 +77,4 @@ else
 fi
 
 exec mariadbd --user=mysql --datadir="$DATADIR" --socket="$SOCKET"
+# exec mysqld_safe
